eab8b535564be5fe0d9150156a84c28d2ad1cb3a
Aman Sharma <asharma8464@gmail.com>
2025-07-07 17:13:59 +0530

    -

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -19,7 +19,7 @@ function App() {
       <h1>Welcome Aman</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+          count is and {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
